19e0de57cda8a39feefdf4c4c9373f31aa038a74
crowegian <oliverbdw4@gmail.com>
2018-04-28 10:05:38 -0400

    plotting for dataset comparison is done

--- src/mlPipeline/plotting.py
+++ src/mlPipeline/plotting.py
@@ -45,3 +45,100 @@ def plotBestModelComparison(modelDictAll, scoring = ["f1", "recall", "precision"
         plt.title("Best Model Based on {} for Metric {}".format(refitMetric.upper(),
                                                                 score.upper()))
     return(scoringDict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def findBestModel(modelList, resultKey, removeZeroF1Score):
+    bestModelName = ""
+    bestPerformance = 0# this assumes we want to maximize performance
+    f1Key = "mean_test_f1"
+    modelStd = -1
+    stdKey = resultKey.replace("mean_test_", "std_test_")
+    for modelName, cvDict in modelList.items():
+    #     print(modelName)
+        currModelIndex = cvDict["gridcv"].best_index_
+        currModelPerformance = cvDict["gridcv"].cv_results_[resultKey][currModelIndex]
+        if currModelPerformance >= bestPerformance:
+#             if removeZeroF1Score and 
+            f1Score = cvDict["gridcv"].cv_results_[f1Key][currModelIndex]
+#             print(f1Score)
+            if removeZeroF1Score and f1Score == 0.0:
+                print("0.0 f1 score found. Model {} is not counted".format(modelName))
+                continue
+            bestModelName = modelName
+            bestPerformance = currModelPerformance
+            modelStd = cvDict["gridcv"].cv_results_[stdKey][currModelIndex]
+    return(bestModelName, bestPerformance, modelStd)
+
+
+
+from collections import defaultdict
+
+def findBestModelPerDataset(allDataModelDict, dataSetComparisonList, compMetric, removeZeroF1Score):
+#     allDataModelDict = modelDictHolder
+#     dataSetComparisonList = list(allDataModelDict.keys())
+#     compMetric = "roc_auc"
+    resultKey = "mean_test_" + compMetric
+#     dataSetComparisonList
+    datasetBestModelDict = {}
+    for datasetName in dataSetComparisonList:
+    #     print(datasetName)
+        modelList = allDataModelDict[datasetName]
+        dataSetBestPerformance  = findBestModel(modelList, resultKey, removeZeroF1Score)
+        datasetBestModelDict[datasetName] = dataSetBestPerformance
+    return(datasetBestModelDict)
+
+
+
+
+
+
+def plotDatasetModelComparison(allDataModelDict, dataSetComparisonList,
+                               compMetric, removeZeroF1Score = True, removeZeroScores = True):
+    datasetBestModelDict = findBestModelPerDataset(allDataModelDict, dataSetComparisonList,
+                                                   compMetric, removeZeroF1Score)
+    modelIdx = 0
+    perfIdx = 1
+    stdidx = 2
+    if removeZeroScores:
+        print("Dataset with all zero f1 scores are being removed from plots")
+        dataSetList = [dset for dset in list(datasetBestModelDict.keys())
+                       if datasetBestModelDict[dset][modelIdx] != ""]
+
+    # dataSetList.sort()
+    perfList = [datasetBestModelDict[dset][perfIdx] for dset in dataSetList]
+    stdList = [datasetBestModelDict[dset][stdidx] for dset in dataSetList]
+    modelList = [datasetBestModelDict[dset][modelIdx] for dset in dataSetList]
+    modelNameMapper = {"GBTC": "GBTC",
+                       "randomForest": "RF",
+                       "logistic": "LG",
+                       "SVC": "SVC"}
+    modelList = [modelNameMapper[mdl] for mdl in modelList]
+    ind = np.arange(len(modelList))
+    if len(dataSetComparisonList) > 8:
+        figSize = (12,6)
+    else:
+        figSize = np.array([9,6])*0.75
+    fig=plt.figure(figsize=figSize, dpi= 300, facecolor='w', edgecolor='k')
+    barBorderWidths = np.array([0]*len(ind))
+    barBorderWidths[np.where(np.array(stdList) == 0)] = 1.5
+    plt.bar(left = ind, height = perfList, yerr = stdList,
+           edgecolor='#763626', linewidth = barBorderWidths, color = "#336B87")
+    xLbls = [dset.replace("_", " ").strip() for dset in dataSetList]
+    xLbls = [lbl + "\n" + mdlName for mdlName, lbl in zip(modelList, xLbls)]
+    plt.xticks(ind, xLbls, rotation = 45, ha = "center")
+    plt.ylim((0,1))
+    title = plt.title("Dataset Performance Comparison")
+    return(datasetBestModelDict)
